94387cd3c6348843ee74bbf9a675d8550421f741
Marcelo Ferrufino <chelo.fm@gmail.com>
2015-11-09 10:20:43 -0400

    Updating conversions to script

--- Marcelo/convert_hours.rb
+++ Marcelo/convert_hours.rb
@@ -1,19 +1,13 @@
 # marcelo ferrufino murillo
 
-# Script to convert hours to minutes/seconds
+# Script to convert minutes to seconds, minutes and days
 
-puts "---------------------------------------"
-puts "- Convert hours to minutes or seconds -"
-puts "---------------------------------------"
-print "enter hours to be converted: "
-hours = gets.chomp
+puts "--------------------------------------------------"
+puts "- Convert minutes to: seconds, minutes and  days -"
+puts "--------------------------------------------------"
+print "Enter minutes to be converted : "
+minutes = gets.chomp
 
-print "do you want convert to MINUTES (enter '1') or convert to SECONDS (enter '2')? _ "
-option = gets.chomp
-if ( option == "1" )
-	puts hours + " hours in minutes are: #{ (hours.to_f * 60.0).round(2) }" 
-elsif ( option == "2" )
-	puts hours + " hours in seconds are: #{ (hours.to_f * 3600.0).round(2) }" 
-else
-	puts "wrong option entered"
-end
+puts minutes + " minutes in seconds are: #{ minutes.to_i * 60 }" 
+puts minutes + " minutes in hours are: #{ (minutes.to_f / 60.0).round(2) }" 
+puts minutes + " minutes in days are: #{ ((minutes.to_f / 60.0) / 24).round(2) }"
