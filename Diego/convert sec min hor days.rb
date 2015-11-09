ce2c3a9e160d4bd011129bc976d6c7406394ee63
unknown <Diego Claure@DCLAUREO-DH01.jalasoft.local>
2015-11-09 16:54:34 -0400

    practica de convertir sec min hor

---
+++ Diego/convert sec min hor days.rb
@@ -0,0 +1,10 @@
+# convertir segundos, minutos y horas
+
+sec = 60
+puts "Seconds = #{sec}"
+min = sec /60
+puts "Minutes = #{min}"
+hor = min /60
+puts "Hours = #{hor}"
+days = hor /24
+puts "days = #{days}"
