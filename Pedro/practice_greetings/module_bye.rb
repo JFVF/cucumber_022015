7ec676e749e9e12147efb9081bf0e559c1aa119a
unknown <Ruby@B202-09.fundacion-jala.local>
2015-11-11 18:33:37 -0400

    Adding the scripts that I've made in the fourth class

---
+++ Pedro/practice_greetings/module_bye.rb
@@ -0,0 +1,15 @@
+module Bye
+
+	def print_good_bye
+		puts 'good bye'
+	end
+
+	def print_see_you_later
+		puts 'see you later'
+	end
+
+	def print_take_care
+		puts 'take care'
+	end
+
+end
---
+++ Pedro/practice_greetings/module_person.rb
@@ -0,0 +1,10 @@
+require './module_greetings'
+require './module_bye'
+
+
+Greetings.print_hi
+Greetings.print_good_night
+
+#Bye.print_see_you_later
+
+
