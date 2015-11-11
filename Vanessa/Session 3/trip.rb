6671e62a1f644c37ea2609a2bbbfc975e55c7661
vanevane <vane.27.04@hotmail.com>
2015-11-11 09:03:39 -0400

    Some more exercises of session 3

--- Vanessa/Session 3/trip.rb
+++ Vanessa/Session 3/trip.rb
@@ -1,20 +1,33 @@
+=begin
+Write 1 method that prints a passenger name ,
+a destination city and the price of ticket in dollars.
+
+The name should be received from the user
+
+If no destination is defined the value by default should be CBBA.
+
+The price of the ticket should be received by user, 
+and the transformation to dollars should be returned from the method. 
+It should be  float (E.g 150.25)
+
+Perform the calculation using variables, to return the value don’t use return word
+When you call the method, don’t use parenthesis
+
+After call the method don’t forget to print the value returned
+=end
+
 def calculate_trip(destination = 'CBBA')
 	puts 'Insert name of the passanger:'
 	name = gets.chomp
 	puts 'Insert price of the ticket'
 	price = gets.chomp.to_f
-	dollar_price = price / 6.8
+	dollar_price = price / 6.9
 
-	# name, destination, dollar_price
-	# name
-	puts "The name of the passanger: #{name}"
+	puts "The name of the passanger is: #{name}"
 	puts "The destination is: #{destination}"
 	puts "The price of the ticket is: #{dollar_price}"
 end
-# calculate_trip
-calculate_trip 'LP'
 
-# trip = calculate_trip
-# puts "Name of the passager #{trip[0]}"
-# puts "Name of the passager #{trip[1]}"
-# puts "Name of the passager #{trip[2]}"
+calculate_trip
+# calculate_trip 'LP' 
+
