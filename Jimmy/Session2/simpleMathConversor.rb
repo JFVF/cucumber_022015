7eef876203febc65f2bb4c78fb892bfa6857f1fe
jfvf <jimmy.freddy.vargas@gmail.com>
2015-11-11 07:14:44 -0400

    add a simpleMathHourConversor

---
+++ Jimmy/Session2/simpleMathConversor.rb
@@ -0,0 +1,11 @@
+#SimpleMathConversor
+
+seconds = 86400
+minutes = seconds/60
+hours = minutes/60
+days = hours/24
+
+puts "Total seconds: #{seconds}"
+puts "Total minutes: #{minutes}"
+puts "Total hours: #{hours}"
+puts "Total days: #{days}"
