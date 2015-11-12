e38e29e859dd54cb4109ce739b0d68a5aa9a2ffe
jfvf <jimmy.freddy.vargas@gmail.com>
2015-11-12 06:00:46 -0400

    renaming the session3 tasks

--- Jimmy/Session3/temperatureConversor_Practice2.rb
+++ Jimmy/Session3/temperatureConversor_Practice2.rb
@@ -3,19 +3,21 @@
 #function to converse Celsius to Fahrenheit
 
 def celsiusToFahrenheit(celsius)
+	celsius = celsius.to_f
 	((9*celsius)/5)+32
 end
 
 def fahrenheitToCelsius(fahrenheit)
+	fahrenheit = fahrenheit.to_f
 	celsius = (5*(fahrenheit-32))/9
 end
 
 #celsius 0 => 32
 print("Enter the celsius: ")
-celsius = gets.chomp.to_f
+celsius = gets.chomp
 puts(celsiusToFahrenheit(celsius))
 
 #fahrenheit 32 => 0
 print("Enter the fahrenheit: ")
-fahrenheit = gets.chomp.to_f
+fahrenheit = gets.chomp
 puts(fahrenheitToCelsius(fahrenheit))
