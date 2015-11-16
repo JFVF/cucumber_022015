7a9b9e0a71da2790bda774cbb63c32c37ed80c04
jfvf <jimmy.freddy.vargas@gmail.com>
2015-11-16 05:23:32 -0400

    completing the list of practices from session 2

---
+++ Jimmy/Session2/simpleMathConversorPractice2.rb
@@ -0,0 +1,13 @@
+#SimpleMathConversor with a variable that doesn't exist
+
+seconds = 86400
+minutes = seconds/60
+hours = minutes/60
+days = hours/24
+
+puts "Total seconds: #{seconds}"
+puts "Total minutes: #{minutes}"
+put "Total years: #{years}"#the variable is undefined and the scripts stops here
+puts "Total hours: #{hours}" 
+puts "Total days: #{days}"
+
