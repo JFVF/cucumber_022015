2825efcea186d76c2919f900551117141824a1bc
unknown <ruby@B202-08.fundacion-jala.local>
2015-11-18 18:21:25 -0400

    session 7

---
+++ Jimmy/Session7/select_hash_sample1.rb
@@ -0,0 +1,13 @@
+#select over hash
+
+salaries = {"bob" =>10.9,
+			"larry"=>7.5,
+			"jimmy"=>6.0,
+			"jerry"=>6.5}
+salaries.inspect
+mySalaryArray = salaries.select{|name,salary| salary > 7.0}
+p mySalaryArray #prints  [["larry", 7.5], ["bob", 10.9]]
+
+mySalaryArray = salaries.select{|name,salary| name == "bob"}
+p mySalaryArray #prints  [["bob", 10.9]]
+
