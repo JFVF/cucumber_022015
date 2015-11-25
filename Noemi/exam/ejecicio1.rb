16a91e9d0065cbcf389b630c5d41d3c0fb124313
unknown <ruby@B202-14.fundacion-jala.local>
2015-11-25 18:45:23 -0400

    examen

---
+++ Noemi/exam/ejecicio1.rb
@@ -0,0 +1,20 @@
+=begin
+autor Noemi Guzman	
+date 11/25/2015
+=end
+
+module Tine_conver
+	def Tine_conver.min_to_hours min_t 
+		puts " #{min_t} min in hours is : #{min_t * 60}"
+	end	
+	def Tine_conver.hour_to_days hour_t
+		puts " #{hour_t} hours in days is : #{hour_t / 24}"
+	end	
+	def Tine_conver.day_to_mouts day_t
+		puts " #{hour_t} days in mounts is : #{day_t /30}"
+	end
+	
+end	
+Tine_conver.min_to_hours 120
+Tine_conver.hour_to_days 240
+Tine_conver.day_to_mouts 90
