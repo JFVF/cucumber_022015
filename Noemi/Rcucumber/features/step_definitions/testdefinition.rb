03335d749496471805f667f61c9e8889efe5a2c8
unknown <ruby@B202-14.fundacion-jala.local>
2015-12-02 18:44:14 -0400

    steps

---
+++ Noemi/Rcucumber/features/step_definitions/testdefinition.rb
@@ -0,0 +1,37 @@
+
+Then(/^I have \$(\d+) in my Account$/) do |arg1|
+  #  pending # Write code here that turns the phrase above into concrete actions
+  # TODO: code goes here
+
+end
+Given(/^I have my password$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+  print "aa"
+end
+#Given /I have deposited \$(\d+) in my Account/ do |arg1|
+# TODO: code goes here
+#end
+Given /I have retired \$(100|250) from my Account/ do |amount|
+# TODO: code goes here
+end
+
+#Given /I have deposited \$(.*) in my Account/ do |amount|
+# TODO: code goes here
+#end
+Given /I have deposited \$([0-9]*) in my Account/ do |amount|
+# TODO: code goes here
+end
+
+Given(/^I had \$(\d+) Accounts$/) do |arg1|
+  #pending # Write code here that turns the phrase above into concrete actions
+  #code
+end
+#only lower case characters
+Given(/^I had ([a-z]*) Account$/) do |arg1|
+  #pending # Write code here that turns the phrase above into concrete actions
+  #code
+end
+#any character except digits.
+Given(/^I had (\D*) password$/) do |arg1|
+ 
+end
---
+++ Noemi/Rcucumber/features/testdefinition.feature
@@ -0,0 +1,17 @@
+Feature: Test definition
+Scenario: example
+#let’s examine the step definition in detail:
+#Considering the Step :
+Given I have $100 in my Account
+Given I have my password
+Given I have deposited $250 in my Account
+Given I have retired $100 from my Account
+#Given I have deposited $1 and a cucumber in my Account
+Given I had $2 Accounts
+Given I had jala Account
+Given I had clase password
+Given I had FUNDACION Account
+
+Given I had clase2015 password
+
+
