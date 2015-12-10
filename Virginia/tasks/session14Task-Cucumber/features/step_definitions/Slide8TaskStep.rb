ee5ae7644e38e486346a02cab13cd0add603093e
svirginia <svirgini05@gmail.com>
2015-12-10 14:39:41 -0400

    slide14 tasks

---
+++ Virginia/tasks/session14Task-Cucumber/features/step_definitions/Slide8TaskStep.rb
@@ -0,0 +1,11 @@
+Given(/^I eat (\d+) apples?$/) do |arg1|
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I have paid \$(\d+) through (\w+)$/) do |arg1|
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I have (?:lunch|dinner) to my home$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
--- Virginia/twelveDay/features/step_definitions/AirportScreensStep.rb
+++ Virginia/twelveDay/features/step_definitions/AirportScreensStep.rb
@@ -6,6 +6,10 @@ Given(/^I have deposited "([^"]*)" in my "([^"]*)" Account$/) do |arg1, arg2|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
+#Given(/^I have deposited \$(\d+) in my (\w+) Account$/) do |amount, account_type|
+  #pending # Write code here that turns the phrase above into concrete actions
+#end
+
 When(/^I transfer \$(\d+) from my Savings Account into my Checking Account$/) do |arg1|
   pending # Write code here that turns the phrase above into concrete actions
 end
