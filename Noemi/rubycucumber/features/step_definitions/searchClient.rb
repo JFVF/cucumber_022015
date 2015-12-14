3bbf14b8923da9fd789565c1e420985c5be2a906
unknown <ruby@B202-14.fundacion-jala.local>
2015-12-14 18:39:18 -0400

    api

--- Noemi/rubycucumber/features/step_definitions/searchClient.rb
+++ Noemi/rubycucumber/features/step_definitions/searchClient.rb
@@ -1,28 +1,21 @@
-Given(/^I have empty list$/) do 
-	$h_u=Hash.new		
-	$h_p=Hash.new
-	$h_u.inspect
-	$h_p.inspect
-	
-  #pending # Write code here that turns the phrase above into concrete actions
-end
-Given(/^I add client (\d+) with name (\w+) and \$(\d+)$/) do |id,name_c,price_c|
-	#@clients.add_client(id.to_i, namec, price.to_i)	
-	@id_c= id.to_i
-	$h_u[@id_c] = name_c.to_s
-	$h_p[@id_c] = price_c.to_s
+Given(/^I have empty list$/) do
+	#$list= Registor.new 
 
+	
 end
+
+Given(/^I add client (\d+) with name (\w+) and \$(\d+)$/) do |id,name_c,price_c|	
+	@id_c= id.to_i
+	registor_example.add_client(@id_c, name_c, price_c)
+end
+
 Given(/^I have (\w+) client$/) do |name|	
 	@name_to_ask=name.to_s
 end
 
 When(/^I ask for TotalPrices$/) do
-	@price_cliente = 0.to_s
-	if $h_u.has_value?(@name_to_ask)
- 		@id_cliente = $h_u.key(@name_to_ask)
- 		@price_cliente = $h_p[@id_cliente]
- 	end
+	#registor_example.print_lists
+	@price_cliente = registor_example.ask @name_to_ask
 end
 Then(/^I should receive \$(\d+)$/) do |price|
  
