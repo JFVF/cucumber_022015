45587ee9dad5bd606ef302339695901772563f92
jfvf <jimmy.freddy.vargas@gmail.com>
2015-12-15 06:28:28 -0400

    session16

---
+++ Jimmy/Session16/features/support/env.rb
@@ -0,0 +1,12 @@
+def find_config_file(filename)
+  root = Pathname.pwd
+  while not root.root?
+    root.find do |path|
+      if path.file? and path.basename.to_s == filename
+        return path.to_s
+      end
+    end
+    root = root.parent
+  end
+  raise 'Configuration file ' #{filename}' not found!'
+end
