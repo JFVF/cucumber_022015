a1896036e06f0e83d9ca179c4331c305531962db
Marcelo Ferrufino <chelo.fm@gmail.com>
2015-12-16 09:03:29 -0400

    updating helper comment

--- Marcelo/project/features/support/helpers/json_handler.rb
+++ Marcelo/project/features/support/helpers/json_handler.rb
@@ -3,7 +3,7 @@ require 'json'
 module JsonHandler
 
   #
-  # This method set up a connection 
+  # This method allows verify if an existing value for a key element is present in json
   #
   def JsonHandler.contains_an_element?(json, key, value)
     hash = JSON.parse(json)
